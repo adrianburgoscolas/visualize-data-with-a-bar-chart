9fcf228132aee2785c779b7808b1b3d3f1e1f823
adrianburgos <adrian.burgos.colas@gmail.com>
2021-11-07 02:32:18 -0500

    all test passed, cool tooltip

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ document.addEventListener('DOMContentLoaded', function(){
         const w = 800;
         const h = 500;
         const padding = 50;
-        
+        // chart
         const xScale = d3.scaleLinear();
         const xData = dataset.map((data)=>parseInt(data[0].split('-')[0],10))
-        console.log(xData)
         xScale.domain([0,dataset.length]).range([padding,w-padding]);
 
         const xAxisScale = d3.scaleLinear();
@@ -25,14 +24,14 @@ document.addEventListener('DOMContentLoaded', function(){
         const hScale = d3.scaleLinear();
         hScale.domain([0,d3.max(dataset,(d)=>d[1])]).range([padding,h-padding]);
 
-        // console.log(hScale(31)-padding);
         const myChart = d3.select('main').select('div')
                         .append('svg')
                         .attr('width',w)
                         .attr('height',h);
 
         let tooltip = d3.select('main')
-            .append('div')
+            .append('p')
+            
             .attr('id','tooltip')   
             .attr('z-index',5)
             .style('visibility','hidden')
@@ -45,8 +44,11 @@ document.addEventListener('DOMContentLoaded', function(){
                 .append('rect')
                 .on('mouseover',(d)=>{
                     tooltip.style('visibility','visible')
-                    tooltip.text(d.target[Object.keys(JSON.parse(JSON.stringify(d.target)))[0]])
-                    tooltip.attr('data-date',d.target[Object.keys(JSON.parse(JSON.stringify(d.target)))[0]][0])
+                    tooltip.text(d.target.__data__)
+                    tooltip.attr('data-date',d.target.__data__[0])
+                })
+                .on('mousemove',(d)=>{
+                    tooltip.style("top", (d.pageY-10)+"px").style("left",(d.pageX+10)+"px")
                 })
                 .on('mouseout',(d)=>{
                     tooltip.style('visibility','hidden')
@@ -58,8 +60,6 @@ document.addEventListener('DOMContentLoaded', function(){
                 .attr('class','bar')
                 .attr('data-date',(d,i)=>d[0])
                 .attr('data-gdp',(d)=>d[1]);
-         
-              
                 
         const xAxis = d3.axisBottom(xAxisScale).tickFormat(d3.format('d'));
         myChart.append('g')
@@ -71,12 +71,8 @@ document.addEventListener('DOMContentLoaded', function(){
             .attr('id','y-axis')
             .attr('transform',`translate(${padding},0)`)
             .call(yAxis);
-        
-            
         }
-        // chart code
-        // const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9, 31,100, 31, 22, 17, 25, 18, 29, 14, 9, 31,100, 22, 17, 9, 31,100, 25, 18, 29, 14, 9];
-    
+      
     
   });
     
