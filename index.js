94690af512330bc57f6f958df454eac8ba45e2a4
adrianburgos <adrian.burgos.colas@gmail.com>
2021-11-07 02:07:07 -0500

    all test suite passed

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ document.addEventListener('DOMContentLoaded', function(){
     xhr.open('GET','https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
     xhr.send();
     xhr.onload = ()=>{
-        const json = JSON.parse(xhr.responseText).data;
+        const dataset = JSON.parse(xhr.responseText).data;
         const w = 800;
-        const h = 600;
+        const h = 500;
         const padding = 50;
-        console.log(json)
+        
         const xScale = d3.scaleLinear();
+        const xData = dataset.map((data)=>parseInt(data[0].split('-')[0],10))
+        console.log(xData)
         xScale.domain([0,dataset.length]).range([padding,w-padding]);
 
+        const xAxisScale = d3.scaleLinear();
+        xAxisScale.domain([d3.min(xData),d3.max(xData)]).range([padding,w-padding]);
+        
         const yScale = d3.scaleLinear();
-        yScale.domain([0,d3.max(dataset)]).range([h-padding,padding]);
+        yScale.domain([0,d3.max(dataset,(d)=>d[1])]).range([h-padding,padding]);
 
         const hScale = d3.scaleLinear();
-        hScale.domain([0,d3.max(dataset)]).range([padding,h-padding]);
+        hScale.domain([0,d3.max(dataset,(d)=>d[1])]).range([padding,h-padding]);
 
         // console.log(hScale(31)-padding);
         const myChart = d3.select('main').select('div')
@@ -26,21 +31,37 @@ document.addEventListener('DOMContentLoaded', function(){
                         .attr('width',w)
                         .attr('height',h);
 
-                        
+        let tooltip = d3.select('main')
+            .append('div')
+            .attr('id','tooltip')   
+            .attr('z-index',5)
+            .style('visibility','hidden')
+            .attr('data-date','')
+            
 
         myChart.selectAll('rect')
                 .data(dataset)
                 .enter()
                 .append('rect')
-                .attr('width',(w-padding*2)/(dataset.length*1.1))
-                .attr('height',(data)=>hScale(data)-padding)
-                .attr('x',(data,i)=>xScale(i))
-                .attr('y',(data,i)=>yScale(data))
+                .on('mouseover',(d)=>{
+                    tooltip.style('visibility','visible')
+                    tooltip.text(d.target[Object.keys(JSON.parse(JSON.stringify(d.target)))[0]])
+                    tooltip.attr('data-date',d.target[Object.keys(JSON.parse(JSON.stringify(d.target)))[0]][0])
+                })
+                .on('mouseout',(d)=>{
+                    tooltip.style('visibility','hidden')
+                })
+                .attr('width',(w-padding*2)/(dataset.length*0.3))
+                .attr('height',(data)=>hScale(data[1])-padding)
+                .attr('x',(data,i)=>xAxisScale(xData[i]))
+                .attr('y',(data,i)=>yScale(data[1]))
                 .attr('class','bar')
-                .append('title')
-                .text((data)=>data);
-
-        const xAxis = d3.axisBottom(xScale);
+                .attr('data-date',(d,i)=>d[0])
+                .attr('data-gdp',(d)=>d[1]);
+         
+              
+                
+        const xAxis = d3.axisBottom(xAxisScale).tickFormat(d3.format('d'));
         myChart.append('g')
             .attr('id','x-axis')
             .attr('transform',`translate(0,${h-padding})`)
@@ -50,9 +71,11 @@ document.addEventListener('DOMContentLoaded', function(){
             .attr('id','y-axis')
             .attr('transform',`translate(${padding},0)`)
             .call(yAxis);
+        
+            
         }
         // chart code
-        const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9, 31,100, 31, 22, 17, 25, 18, 29, 14, 9, 31,100, 22, 17, 9, 31,100, 25, 18, 29, 14, 9];
+        // const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9, 31,100, 31, 22, 17, 25, 18, 29, 14, 9, 31,100, 22, 17, 9, 31,100, 25, 18, 29, 14, 9];
     
     
   });
